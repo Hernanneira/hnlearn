cd31c184763dd44d838000ba33adbb2872bc5447
Hernan Neira <hernanneira@live.com.ar>
2022-06-05 20:31:45 -0300

    correcion de gramatica y textos

--- js/index.js
+++ js/index.js
@@ -74,9 +74,9 @@
 // console.log(presupuesto)
 
 
-// let nombreUsuario = prompt("BIENVENDIOS! \n Por favor ingrese su nombre");
-// alert("Hola " + nombreUsuario + "\nA continuación realizaremos la solicitud con el presupuesto estimado para el servicio de tu empresa")
-let cantidadDeServicios = prompt("Porfavor selecione la CANTIDAD de Servicios que, \n 1 = Sistema de autoproteccion \n 2 = Certificado de Ignifugado \n 3 = Asesoramiento Legal \n 4 = Presentacion ART \n 5 = Estuduos Higienicos \n 6 = Programas de Seguridad o ananlisis de riesgos");
+let nombreUsuario = prompt("BIENVENDIOS! \n Por favor ingrese su nombre");
+alert("Hola " + nombreUsuario + "\nA continuación realizaremos la solicitud con el presupuesto estimado para el servicio de tu empresa.")
+let cantidadDeServicios = prompt("Ingrese la CANTIDAD(1 o 2 o 3 o ...) de Servicios que desea cotizar, Ej: quiero un sistema de autoproteccion y una capacitacion coloque = 2 \n SERVICIOS: \n Sistema de autoproteccion \n Certificado de Ignifugado \n Asesoramiento Legal \n Presentacion ART \n Estuduos Higienicos \n Programas de Seguridad o ananlisis de riesgos");
 let servicios 
 let instalacion;
 let presupuesto = 0;
@@ -86,7 +86,7 @@ function suma (precio) {
 }
 
 for (i = 1; i <= cantidadDeServicios; i++) {
-   let servicios = prompt("Estos son los servicios que brindamos, porfavor selecciona que servicios necesitas \n 1 = Sistema de autoproteccion \n 2 = Certificado de Ignifugado \n 3 = Asesoramiento Legal \n 4 = Presentacion ART \n 5 = Estuduos Higienicos \n 6 = Programas de Seguridad o ananlisis de riesgos");
+   let servicios = prompt("Ingrese el numero de servicio que quisiera cotizar. \n 1 = Sistema de autoproteccion \n 2 = Certificado de Ignifugado \n 3 = Asesoramiento Legal \n 4 = Presentacion ART \n 5 = Estuduos Higienicos \n 6 = Programas de Seguridad o ananlisis de riesgos");
    switch (servicios) {
       case "1":
          suma (30000)
@@ -107,9 +107,9 @@ for (i = 1; i <= cantidadDeServicios; i++) {
          suma (25000)
          break
       default:
-         alert("Error del valor ingresado.")
+         alert("Error del valor ingresado. suma 0")
          break
    }
 }
 
-console.log(presupuesto)
+alert("La cotización para "+ nombreUsuario +" es de "+ presupuesto + " Pesos")
